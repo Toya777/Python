72e6e2e328625fdc872094fc4c1d88078e605b2e
Guilherme Martins <guilhermemar0728@gmail.com>
2024-07-17 17:18:06 -0300

    Alteração de int para float para a calculadora poder fazer calculo de %

--- Exercícios/Bloco-de-nota-01.py
+++ Exercícios/Bloco-de-nota-01.py
@@ -1,17 +1,24 @@
-resultado = int
+resultado = float
 print("\n---------CALCULADORA---------")
 
-num01 = int(input("\nDigite o primeiro numero: "))
+num01 = float(input("\nDigite o primeiro numero: "))
 sinal = str(input("\nDigite o sinal: "))
-num02 = int(input("\nDigite o segundo numero: "))
+num02 = float(input("\nDigite o segundo numero: "))
 
 if(sinal == "-"):
     resultado = num01 - num02
-    print("\nO resultado é: ", resultado)
-    
+    print("\nO resultado é: ", resultado)    
 elif(sinal == "+"):
     resultado = num01 + num02
     print("\nO resultado é: ", resultado)
-    
+elif(sinal == "*"):
+    resultado = num01 * num02
+    print("\nO resultado é: ", resultado)
+elif(sinal == "/"):
+    resultado = num01 / num02
+    print("\nO resultado é: ", resultado)
+elif(sinal == "%"):
+    resultado = num01 % num02
+    print("\nO resultado é: ", resultado)
 else:
     print("\nSinal invalido")
