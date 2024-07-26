8bcf384e9b85fc255435aecc88501f45cd405b0c
Guilherme Martins <guilhermemar0728@gmail.com>
2024-07-26 13:41:56 -0300

    Atualização no codigo

--- Exercícios/Bloco-de-nota-01.py
+++ Exercícios/Bloco-de-nota-01.py
@@ -1,24 +1,28 @@
 resultado = float
 print("\n---------CALCULADORA---------")
 
-num01 = float(input("\nDigite o primeiro numero: "))
-sinal = str(input("\nDigite o sinal: "))
-num02 = float(input("\nDigite o segundo numero: "))
+for i in range(1000000):
+    num01 = float(input("\nDigite o primeiro numero: "))
+    sinal = str(input("\nDigite o sinal: "))
+    num02 = float(input("\nDigite o segundo numero: ")) 
+    if(sinal == "-"):
+        resultado = num01 - num02
+        print("\nO resultado é: ", resultado)    
+    elif(sinal == "+"):
+        resultado = num01 + num02
+        print("\nO resultado é: ", resultado)
+    elif(sinal == "*"):
+        resultado = num01 * num02
+        print("\nO resultado é: ", resultado)
+    elif(sinal == "/"):
+        resultado = num01 / num02
+        print("\nO resultado é: ", resultado)
+    elif(sinal == "%"):
+        resultado = num01 % num02
+        print("\nO resultado é: ", resultado)
+    else:
+        print("\nSinal invalido")
 
-if(sinal == "-"):
-    resultado = num01 - num02
-    print("\nO resultado é: ", resultado)    
-elif(sinal == "+"):
-    resultado = num01 + num02
-    print("\nO resultado é: ", resultado)
-elif(sinal == "*"):
-    resultado = num01 * num02
-    print("\nO resultado é: ", resultado)
-elif(sinal == "/"):
-    resultado = num01 / num02
-    print("\nO resultado é: ", resultado)
-elif(sinal == "%"):
-    resultado = num01 % num02
-    print("\nO resultado é: ", resultado)
-else:
-    print("\nSinal invalido")
+    avancar = input("Digite nao caso nao queira continuar: ")
+    if(avancar == "nao"):
+        break
